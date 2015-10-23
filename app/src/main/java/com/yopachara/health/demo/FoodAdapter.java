d8299b05d0d575d6e67f4bdec417e1b45856b755
Pachara S <pachara.s@mail.kmutt.ac.th>
2015-10-24 02:46:21 +0700

    Add function search in FoodFragment and add function manage recycler view in FoodAdapter

--- app/src/main/java/com/yopachara/health/demo/FoodAdapter.java
+++ app/src/main/java/com/yopachara/health/demo/FoodAdapter.java
@@ -2,13 +2,12 @@ package com.yopachara.health.demo;
 
 import android.content.Context;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.yopachara.health.demo.Model.FoodModel;
 
@@ -20,7 +19,7 @@ import java.util.List;
  */
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 
-    private List<FoodModel.Foods> mPlayers;
+    private final List<FoodModel.Foods> mPlayers;
     private Context mContext;
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -34,9 +33,10 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
             name = (TextView) view.findViewById(R.id.name);
             type = (TextView) view.findViewById(R.id.club);
         }
+
         @Override
         public void onClick(View view) {
-            Snackbar.make(view.getRootView(),"position = " + getPosition()+" "+name.getText(), Snackbar.LENGTH_LONG).show();
+            Snackbar.make(view.getRootView(), "position = " + getPosition() + " " + name.getText(), Snackbar.LENGTH_LONG).show();
         }
     }
 
@@ -66,5 +66,61 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
     @Override
     public int getItemCount() {
         return mPlayers.size();
+    }
+
+//    public void setModels(List<FoodModel.Foods> models) {
+//        mPlayers = new ArrayList<>(models);
+//    }
+
+    public void animateTo(List<FoodModel.Foods> models) {
+        applyAndAnimateRemovals(models);
+        applyAndAnimateAdditions(models);
+        applyAndAnimateMovedItems(models);
+        Log.d("animateTo",getItemCount()+"");
+    }
+
+    private void applyAndAnimateRemovals(List<FoodModel.Foods> newModels) {
+        for (int i = mPlayers.size() - 1; i >= 0; i--) {
+            final FoodModel.Foods model = mPlayers.get(i);
+            if (!newModels.contains(model)) {
+                removeItem(i);
+            }
+        }
+    }
+
+    private void applyAndAnimateAdditions(List<FoodModel.Foods> newModels) {
+        for (int i = 0, count = newModels.size(); i < count; i++) {
+            final FoodModel.Foods model = newModels.get(i);
+            if (!mPlayers.contains(model)) {
+                addItem(i, model);
+            }
+        }
+    }
+
+    private void applyAndAnimateMovedItems(List<FoodModel.Foods> newModels) {
+        for (int toPosition = newModels.size() - 1; toPosition >= 0; toPosition--) {
+            final FoodModel.Foods model = newModels.get(toPosition);
+            final int fromPosition = mPlayers.indexOf(model);
+            if (fromPosition >= 0 && fromPosition != toPosition) {
+                moveItem(fromPosition, toPosition);
+            }
+        }
+    }
+
+    public FoodModel.Foods removeItem(int position) {
+        final FoodModel.Foods model = mPlayers.remove(position);
+        notifyItemRemoved(position);
+        return model;
+    }
+
+    public void addItem(int position, FoodModel.Foods model) {
+        mPlayers.add(position, model);
+        notifyItemInserted(position);
+    }
+
+    public void moveItem(int fromPosition, int toPosition) {
+        final FoodModel.Foods model = mPlayers.remove(fromPosition);
+        mPlayers.add(toPosition, model);
+        notifyItemMoved(fromPosition, toPosition);
     }
 }
--- app/src/main/java/com/yopachara/health/demo/FoodFragment.java
+++ app/src/main/java/com/yopachara/health/demo/FoodFragment.java
@@ -2,12 +2,18 @@ package com.yopachara.health.demo;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,6 +22,7 @@ import com.yopachara.health.demo.Model.FoodModel;
 import com.yopachara.health.demo.Service.HealthService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -23,15 +30,26 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 
-public class FoodFragment extends Fragment {
+public class FoodFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView mRecyclerView;
     private FoodAdapter mAdapter;
-    private FoodModel foodModel;
+    private FoodModel foodModels;
     String API = "http://pachara.me:3000";
     SnackBar mSnackBar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
+    ArrayList<FoodModel.Foods> foods= new ArrayList<FoodModel.Foods>();
+    private String queryDecrease = "";
+    private String queryTmp;
+    private Handler mHandler = new Handler();
+    Runnable mFilterTask = new Runnable() {
+
+        @Override
+        public void run() {
+            getFoodSearch(queryTmp);
+        }
+    };
 
     public static FoodFragment newInstance() {
         FoodFragment fragment = new FoodFragment();
@@ -40,10 +58,10 @@ public class FoodFragment extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_food, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.foodList);
         getFoods(v);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeFoodContainer);
@@ -53,35 +71,32 @@ public class FoodFragment extends Fragment {
                 getFoods(v);
             }
         });
+        setHasOptionsMenu(true);
+
 
         return v;
     }
 
-    public void getFoods(final View v){
+    public void getFoods(final View v) {
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(API).build();
         HealthService api = restAdapter.create(HealthService.class);
 
-        api.getFoods( new Callback<FoodModel>() {
+        api.getFoods(new Callback<FoodModel>() {
 
 
             @Override
             public void success(FoodModel foodModel, Response response) {
-                ArrayList<FoodModel.Foods> foods = foodModel.getObjects();
+                foodModels = foodModel;
+                foods.addAll(foodModel.getObjects());
+                Log.d("Success", "Foods size " + foods.size());
 
-				Log.d("Success", "Foods size "+foods.size());
 
-
-
-                mRecyclerView = (RecyclerView) v.findViewById(R.id.foodList);
-
-                mRecyclerView.setHasFixedSize(true);
                 mLayoutManager = new LinearLayoutManager(v.getContext());
                 mRecyclerView.setLayoutManager(mLayoutManager);
 
-                mAdapter = new FoodAdapter(v.getContext() , foods);
+                mAdapter = new FoodAdapter(v.getContext(), foods);
                 mRecyclerView.setAdapter(mAdapter);
-                mRecyclerView.setHasFixedSize(true);
                 mSwipeRefreshLayout.setRefreshing(false);
 
 
@@ -96,6 +111,82 @@ public class FoodFragment extends Fragment {
             }
 
         });
+    }
+
+    public void getFoodSearch(final String query) {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API).build();
+        HealthService api = restAdapter.create(HealthService.class);
+        api.getFoods(new Callback<FoodModel>() {
+
+            @Override
+            public void success(FoodModel foodModel, Response response) {
+                foodModels = foodModel;
+                foods = foodModel.getObjects();
+                Log.d("Success", "Foods size " + foods.size());
+
+                final List<FoodModel.Foods> filteredModelList = filter(foods, query);
+                mAdapter.animateTo(filteredModelList);
+                mRecyclerView.scrollToPosition(0);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+                Log.d("Error", error.toString());
+                mSwipeRefreshLayout.setRefreshing(false);
+
+            }
+
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_food, menu);
+        final MenuItem item = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        Log.d("onQueqyTextChange", query);
+
+        if (query.length()<queryDecrease.length()){
+            queryTmp = query;
+            mHandler.removeCallbacks(mFilterTask);
+            mHandler.postDelayed(mFilterTask, 500);
+        }
+        else {
+            final List<FoodModel.Foods> filteredModelList = filter(foods, query);
+            mAdapter.animateTo(filteredModelList);
+            mRecyclerView.scrollToPosition(0);
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.d("onQueryTextSubmit", query);
+        return false;
+    }
+
+    private List<FoodModel.Foods> filter(List<FoodModel.Foods> models, String query) {
+        Log.d("filter", query);
+        query = query.toLowerCase();
+        queryDecrease = query;
+        final List<FoodModel.Foods> filteredModelList = new ArrayList<>();
+        for (FoodModel.Foods model : models) {
+            final String text = model.getName().toLowerCase();
+            if (text.contains(query)) {
+                filteredModelList.add(model);
+            }
+        }
+        Log.d("Filter Size", filteredModelList.size() + "");
+        return filteredModelList;
     }
 
 
